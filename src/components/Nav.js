87f578f9ad7dfc2d168b4068a7068882f88e67bb
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2020-12-31 18:48:32 +0000

    add aria-current tag to current page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,15 +11,27 @@ const Nav = ({ nav, pageUrl }) => {
       <ul class="topnav">
         ${sortedNav(nav).map((navItem) => {
           const isCurrent = navItem.url == pageUrl;
-          return html`<li
-            class=${`topnav--item${isCurrent ? "-selected" : ""}`}
-          >
-            <a href=${navItem.url}>${navItem.data.title}</a>
-          </li>`;
+          return isCurrent
+            ? html`<${CurrentNavItem}
+                url=${navItem.url}
+                title=${navItem.data.title}
+              />`
+            : html`<${NavItem}
+                url=${navItem.url}
+                title=${navItem.data.title}
+              />`;
         })}
       </ul>
     </nav>
   `;
 };
+
+const CurrentNavItem = ({ url, title }) =>
+  html`<li class="topnav--item-selected">
+    <a aria-current="page" href=${url}>${title}</a>
+  </li>`;
+
+const NavItem = ({ url, title }) =>
+  html`<li class="topnav--item"><a href=${url}>${title}</a></li>`;
 
 module.exports = { Nav };
