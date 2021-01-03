ae30c49546bc84f42e5de48738c144e54205f17c
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-01-03 09:13:24 +0000

    show latest posts on index

--- src/pages/index.11ty.js
+++ src/pages/index.11ty.js
@@ -1,3 +1,7 @@
+const { html } = require("htm/preact");
+const { render } = require("preact-render-to-string");
+const { formatDistanceToNow } = require("date-fns");
+
 exports.data = {
   layout: "base",
   title: "Home",
@@ -6,6 +10,35 @@ exports.data = {
   navorder: 1,
 };
 
+const getLatestPosts = (data, numberOfPosts = 5) =>
+  [...data.collections.blog]
+    .reverse()
+    .filter((post) => post.data.page.url != data.page.url)
+    .slice(0, numberOfPosts);
+
 exports.render = function (data) {
-  return "<h1>index</h1>";
+  return render(html`<section class="featured">
+    <h3 class="featured__title">Latest Posts</h3>
+    <ul class="featured-list">
+      ${getLatestPosts(data, 3).map(
+        (post) => html`
+          <li class="featured-list__item">
+            <h5 class="featured-list__title">
+              <a class="featured-list__link" href=${post.data.page.url}>
+                ${post.data.title}
+              </a>
+            </h5>
+            <time class="featured-list__timestamp">
+              ${formatDistanceToNow(post.data.page.date)} ago
+            </time>
+          </li>
+        `
+      )}
+    </ul>
+    <div class="featured-moreposts">
+      <h4 class="featured-moreposts__title">
+        <a class="featured-moreposts__link" href="/blog/">All Posts</a>
+      </h4>
+    </div>
+  </section>`);
 };
