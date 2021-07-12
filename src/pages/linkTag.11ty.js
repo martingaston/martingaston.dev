4d2ce6138ff8baef7b1e56ab978449f579475b2f
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-07-12 21:36:32 +0100

    add Links page and linkTag page (to generate tag links)

---
+++ src/pages/linkTag.11ty.js
@@ -0,0 +1,55 @@
+const { html } = require("htm/preact");
+const { render } = require("preact-render-to-string");
+
+const { Title } = require("../components/Title");
+const { LinkItem } = require("../components/LinkItem");
+
+exports.data = {
+  layout: "base",
+  pagination: {
+    data: "collections.links",
+    size: 1,
+    alias: "tag",
+    before: getUniqueTagsFromCollection,
+  },
+  permalink: function (data) {
+    return `links/tags/${this.slug(data.tag)}/`;
+  },
+  eleventyComputed: {
+    title: ({ tag }) => capitalise(tag),
+  },
+};
+
+function getUniqueTagsFromCollection(data) {
+  const uniqueTags = new Set();
+
+  for (let template of data) {
+    for (let tag of template.data.tags) {
+      uniqueTags.add(tag);
+    }
+  }
+
+  return [...uniqueTags];
+}
+
+function capitalise(string) {
+  return string
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+exports.render = function (data) {
+  return render(
+    html`
+      <${Title} title="${capitalise(data.tag)} Links" />
+      <article class="content">
+        <ul>
+          ${[...data.collections[data.tag]]
+            .reverse()
+            .map((link) => html`<${LinkItem} link=${link} />`)}
+        </ul>
+      </article>
+    `
+  );
+};
---
+++ src/pages/links.11ty.js
@@ -0,0 +1,26 @@
+const { html } = require("htm/preact");
+const { render } = require("preact-render-to-string");
+
+const { Title } = require("../components/Title");
+const { LinkItem } = require("../components/LinkItem");
+
+exports.data = {
+  layout: "base",
+  title: "Links",
+  permalink: "links/index.html",
+  tags: "nav",
+  navorder: 3,
+};
+
+exports.render = function (data) {
+  return render(
+    html`<${Title} title="Links" />
+      <article class="content">
+        <ol class="postlist">
+          ${[...data.collections.links]
+            .reverse()
+            .map((link) => html`<${LinkItem} link=${link} />`)}
+        </ol>
+      </article>`
+  );
+};
