24de8b8628cebdb4f79c68d08d86b9692317bf13
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-07-13 12:40:17 +0100

    Add list of tags to link aggregate page

--- src/pages/links.11ty.js
+++ src/pages/links.11ty.js
@@ -3,6 +3,7 @@ const { render } = require("preact-render-to-string");
 
 const { Title } = require("../components/Title");
 const { LinkItem } = require("../components/LinkItem");
+const { Tags } = require("../components/Tags");
 
 exports.data = {
   layout: "base",
@@ -16,6 +17,10 @@ exports.render = function (data) {
   return render(
     html`<${Title} title="Links" />
       <article class="content">
+        <${Tags}
+          tags=${getAllTagsFromCollection(data.collections.links)}
+          prefix="/links/tags"
+        />
         <ol class="postlist">
           ${[...data.collections.links.all]
             .reverse()
@@ -24,3 +29,7 @@ exports.render = function (data) {
       </article>`
   );
 };
+
+function getAllTagsFromCollection(collection) {
+  return Object.keys(collection).filter((tag) => tag !== "all");
+}
